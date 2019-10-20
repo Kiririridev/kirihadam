330202c8fe0a1fad19d000f88b1492ecb387d604
Kiririridev <bartek.kirej@gmail.com>
2019-10-20 21:13:03 +0200

    Initial commit

---
+++ src/main/java/com/kirihadam/kirihadam/KirihadamKafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.kirihadam.kirihadam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirihadamKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirihadamKafkaStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirihadam/kirihadam/KirihadamKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirihadam.kirihadam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirihadamKafkaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
